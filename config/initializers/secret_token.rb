634d9323fd30c13fefc49a24d93ebeb002ee0732
Alberto <bertojanza@yahoo.es>
2012-01-13 20:58:53 -0500

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBookshelf::Application.config.secret_token = 'cff775ce564faecb8ba1f457ce0db96477315febfe81105b66819afbac120be21e2aef8be876239cbb7c4670ba66329ac3b8c54f5a776bcc609305cd32d58e10'
