02f7a842f2e93f90bee691c61ec3326682b12c22
Alberto <bertojanza@yahoo.es>
2012-02-25 19:02:02 -0500

    Book item CSS. Internationalization of confirm messages.
      Book item CSS to make fit the confirm_message in the item.

--- app/helpers/experiences_helper.rb
+++ app/helpers/experiences_helper.rb
@@ -3,15 +3,27 @@ module ExperiencesHelper
   def confirm_message(code)
     case code
       when 0
-        'In your read bookcase'
+        I18n.t 'in_your_read_bookshelf'
       when 1
-        'In your reading bookcase'
+        I18n.t 'in_your_reading_bookshelf'
       when 2
-        'In your next bookcase'
+        I18n.t 'in_your_next_bookshelf'
       when 3
-        'Recommended'
+        I18n.t 'in_your_recommended_bookshelf'
     end
+  end
 
+  def big_confirm_message(code)
+    case code
+      when 0
+        I18n.t 'in_your_read_bookshelf_big'
+      when 1
+        I18n.t 'in_your_reading_bookshelf_big'
+      when 2
+        I18n.t 'in_your_next_bookshelf_big'
+      when 3
+        I18n.t 'in_your_recommended_bookshelf_big'
+    end
   end
 
 end
